2f207fc2dacae78598646b3d3769b91c01dc745f
Nuttanan29445 <nuch29445@gmail.com>
2024-11-11 21:54:31 +0700

    changeConnectDB

--- easyrice-backend/config/db.js
+++ easyrice-backend/config/db.js
@@ -3,9 +3,19 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) {
+    console.log("DB already connected");
+    return;
+  }
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
     console.log("DB Connected");
   } catch (error) {
     console.error("DB Connection Error:", error);
